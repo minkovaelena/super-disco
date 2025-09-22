e3c667b77e4174d49357733553fc3a92d597003e
minkovaelena <minkovaelena@users.noreply.github.com>
2025-09-22 13:47:00 +0000

    Update test coverage

--- README.md
+++ README.md
@@ -267,3 +267,6 @@ Documentation for README.md
 
 ## Update 4
 
+
+## Update 5
+
--- utils/helper5.py
+++ utils/helper5.py
@@ -1,4 +1,4 @@
-"""Utility functions for repos_super-disco"""
+"""Utility functions for repos_hist_super-disco_20250922"""
 
 import datetime
 from typing import Optional
